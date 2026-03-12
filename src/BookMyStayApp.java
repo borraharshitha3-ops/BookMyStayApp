842002e1f6f1e7c24c5350078f02e750656450b2
borraharshitha3-ops <borraharshitha3@gmail.com>
2026-03-12 10:37:54 +0530

    UC1 - Display available hotels

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,18 @@
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to BookMyStay App");
+
+        showHotels();
+    }
+
+    public static void showHotels() {
+
+        System.out.println("Available Hotels:");
+        System.out.println("1. Taj Hotel");
+        System.out.println("2. ITC Grand");
+        System.out.println("3. Oberoi Hotel");
+
+    }
 }
